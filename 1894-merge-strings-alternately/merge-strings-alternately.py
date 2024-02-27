8f6f0dad52f5ae174630d7421633b6a779806fd7
Shruti Varade <37963866+shrutivarade@users.noreply.github.com>
2024-02-27 13:51:44 -0500

    Time: 41 ms (25.30%) | Memory: 16.5 MB (72.00%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.py
+++ 1894-merge-strings-alternately/merge-strings-alternately.py
@@ -1,19 +1,36 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
 
-        i=0
-        j=0
-        word3 = ''
-        while((i<len(word1) and (j<len(word2)))):
-            word3 = word3 + word1[i] + word2[j]
-            i += 1
-            j += 1
+        # using string operations
+        # i=0
+        # j=0
+        # word3 = ''
+        # while((i<len(word1) and (j<len(word2)))):
+        #     word3 = word3 + word1[i] + word2[j]
+        #     i += 1
+        #     j += 1
           
-        while(i<len(word1)):
-            word3 = word3 + word1[i]
-            i += 1
-        while(j<len(word2)):
-            word3 = word3 + word2[j]
-            j += 1
-        return word3
+        # while(i<len(word1)):
+        #     word3 = word3 + word1[i]
+        #     i += 1
+        # while(j<len(word2)):
+        #     word3 = word3 + word2[j]
+        #     j += 1
+        # return word3
+
+        # Using array 
+        ans = []
+        i, j = 0, 0
+
+        while i< len(word1) and j < len(word2):
+            ans.append(word1[i])
+            ans.append(word2[j])
+            i +=1
+            j +=1
+        
+        ans.append(word1[i:])
+        ans.append(word2[j:])
+        return ''.join(ans)
+
+
         
