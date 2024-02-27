54e4dd285dad60b035965577a59369eaa2641606
Shruti Varade <37963866+shrutivarade@users.noreply.github.com>
2024-02-27 16:13:12 -0500

    Time: 41 ms (91.70%) | Memory: 17.5 MB (69.14%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.py
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.py
@@ -1,43 +1,25 @@
-# class Solution:
-#     def reverseVowels(self, s: str) -> str:
-
-#         s = list(s)
-#         n = len(s)
-
-#         vowels = set(['a', 'e', 'i', 'o', 'u'])
-
-#         i = 0
-#         j = n-1
-
-#         while i < j:
-             
-#             if ( s[i] in vowels and s[j] in vowels):
-#                 s[i] , s[j] = s[j] , s[i]
-#                 i+=1
-#                 j-=1
-
-#             else:
-#                 if(s[i] not in vowels):
-#                     i += 1
-#                 if(s[j] not in vowels):
-#                     j -= 1
-            
-#         return ''.join(s)
-
 class Solution:
     def reverseVowels(self, s: str) -> str:
-        s=list(s)
-        n=len(s)
-        left=0
-        right=n-1
-        vowels=set('AEIOUaeiou')
-        while left<right:
-            while left<right and s[left] not in vowels:
-                left+=1
-            while left<right and s[right] not in vowels:
-                right-=1
-            s[left],s[right]=s[right],s[left]
-            left+=1
-            right-=1
-        s=''.join(s)
-        return s
+
+        s = list(s)
+        n = len(s)
+
+        vowels = set(['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'])
+
+        i = 0
+        j = n-1
+
+        while i < j:
+             
+            if ( s[i] in vowels and s[j] in vowels):
+                s[i] , s[j] = s[j] , s[i]
+                i+=1
+                j-=1
+
+            else:
+                if(s[i] not in vowels):
+                    i += 1
+                if(s[j] not in vowels):
+                    j -= 1
+            
+        return ''.join(s)
