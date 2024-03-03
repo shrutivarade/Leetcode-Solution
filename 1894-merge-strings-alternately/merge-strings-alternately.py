a44b003596a0d5c37dd775448639a5913f1f4246
Shruti Varade <37963866+shrutivarade@users.noreply.github.com>
2024-03-03 15:35:46 -0500

    Time: 32 ms (77.86%) | Memory: 16.5 MB (67.04%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.py
+++ 1894-merge-strings-alternately/merge-strings-alternately.py
@@ -2,18 +2,18 @@ class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
 
         # Using array 
-        ans = []
-        i, j = 0, 0
+        # ans = []
+        # i, j = 0, 0
 
-        while i < len(word1) and j < len(word2):
-            ans.append(word1[i])
-            ans.append(word2[j])
-            i +=1
-            j +=1
+        # while i < len(word1) and j < len(word2):
+        #     ans.append(word1[i])
+        #     ans.append(word2[j])
+        #     i +=1
+        #     j +=1
         
-        ans.append(word1[i:])
-        ans.append(word2[j:])
-        return ''.join(ans)
+        # ans.append(word1[i:])
+        # ans.append(word2[j:])
+        # return ''.join(ans)
 
         # using string operations
         # i=0
@@ -33,15 +33,15 @@ class Solution:
 
         # Using min() and Slicing
         
-        # n1 = len(word1)
-        # n2 = len(word2)
-        # result = ""
-        # for i in range(0,min(n1,n2)):
-        #     result = result + word1[i] + word2[i]
+        n1 = len(word1)
+        n2 = len(word2)
+        result = ""
+        for i in range(0,min(n1,n2)):
+            result = result + word1[i] + word2[i]
 
-        # if(n1 < n2):
-        #     result = result + word2[n1:]
-        # else:
-        #     result = result + word1[n2:]
+        if(n1 < n2):
+            result = result + word2[n1:]
+        else:
+            result = result + word1[n2:]
         
-        # return result
+        return result
