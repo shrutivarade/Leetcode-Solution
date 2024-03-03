0971aad2e182ca6893eda66d817a3bd860911a0f
Shruti Varade <37963866+shrutivarade@users.noreply.github.com>
2024-03-03 15:33:45 -0500

    Time: 33 ms (74.38%) | Memory: 16.5 MB (67.04%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.py
+++ 1894-merge-strings-alternately/merge-strings-alternately.py
@@ -1,28 +1,11 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
 
-        # using string operations
-        # i=0
-        # j=0
-        # word3 = ''
-        # while((i<len(word1) and (j<len(word2)))):
-        #     word3 = word3 + word1[i] + word2[j]
-        #     i += 1
-        #     j += 1
-          
-        # while(i<len(word1)):
-        #     word3 = word3 + word1[i]
-        #     i += 1
-        # while(j<len(word2)):
-        #     word3 = word3 + word2[j]
-        #     j += 1
-        # return word3
-
         # Using array 
         ans = []
         i, j = 0, 0
 
-        while i< len(word1) and j < len(word2):
+        while i < len(word1) and j < len(word2):
             ans.append(word1[i])
             ans.append(word2[j])
             i +=1
@@ -32,5 +15,33 @@ class Solution:
         ans.append(word2[j:])
         return ''.join(ans)
 
+        # using string operations
+        # i=0
+        # j=0
+        # word3 = ''
+        # while((i<len(word1) and (j<len(word2)))):
+        #     word3 = word3 + word1[i] + word2[j]
+        #     i += 1
+        #     j += 1
+        # while(i<len(word1)):
+        #     word3 = word3 + word1[i]
+        #     i += 1
+        # while(j<len(word2)):
+        #     word3 = word3 + word2[j]
+        #     j += 1
+        # return word3
 
+        # Using min() and Slicing
         
+        n1 = min(len(word1))
+        n2 = min(len(word2))
+
+        for i in range(0,min(n1,n2)):
+            result = result + word1[i] + word2[i]
+
+        if(n1 < n2):
+            result = result + word2[n1:]
+        else:
+            result = result + word1[n2:]
+        
+        return result
