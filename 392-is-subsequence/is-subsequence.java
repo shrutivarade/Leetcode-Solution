8033c72abb499e5eff5fdf618b3242dafa3a7317
Shruti Varade <37963866+shrutivarade@users.noreply.github.com>
2024-03-07 20:30:59 -0500

    Time: 2 ms (59.92%) | Memory: 41.5 MB (40.75%) - LeetSync

--- 392-is-subsequence/is-subsequence.java
+++ 392-is-subsequence/is-subsequence.java
@@ -1,23 +1,47 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
 
-            String count = "";
-            int j=0;
+            // String count = "";
+            // int j=0;
 
-            for(int i=0; i<s.length(); i++){
-                while( j<t.length()){
-                    if(s.charAt(i) == t.charAt(j)){
-                        count = count + s.charAt(i);
-                        j = j+1;
-                        break;
-                    }
-                    j = j+1;
+            // for(int i=0; i<s.length(); i++){
+            //     while( j<t.length()){
+            //         if(s.charAt(i) == t.charAt(j)){
+            //             count = count + s.charAt(i);
+            //             j = j+1;
+            //             break;
+            //         }
+            //         j = j+1;
+            //     }
+            // }
+
+            // if(s.equals(count)){
+            //     return true;
+            // }
+            // else{
+            //     return false;
+            // }
+            
+            
+            int i=0, j=0;
+
+            while(i<s.length() && j<t.length()){
+
+                if(s.charAt(i) == t.charAt(j)){
+                    i++;
+                    j++;
+                }
+                else{
+                    j++;
                 }
             }
 
-            if(s.equals(count)){
+            if(i == s.length()){
                 return true;
             }
+            // if(j == t.length()){
+            //     return false;
+            // }
             else{
                 return false;
             }
