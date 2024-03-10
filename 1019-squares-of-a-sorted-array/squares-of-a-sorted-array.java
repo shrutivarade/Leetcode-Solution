37036413484a6709c3eba1697d70c50037e8b7d3
Shruti Varade <37963866+shrutivarade@users.noreply.github.com>
2024-03-09 21:39:42 -0500

    Time: 1 ms (100.00%) | Memory: 47.5 MB (12.93%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
@@ -1,12 +1,36 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
 
-        for(int i=0; i<nums.length; i++){
-            nums[i] = Math.abs(nums[i] * nums[i]);
+        // solution 1: O(n log n)
+
+        // for(int i=0; i<nums.length; i++){
+        //     nums[i] = Math.abs(nums[i] * nums[i]);
+        // }
+        
+        // Arrays.sort(nums);
+        // return nums;
+        
+
+        //  Squaring each element and sorting the new array is very trivial, could you find an O(n) solution using a different approach?
+
+        // Solution 2: O(n)
+        // this solution work in reverse order because we already have a sorted array
+
+        int len = nums.length;
+        int[] result = new int[len];
+        int rightIndex = len - 1;
+        int leftIndex = 0;
+
+        for(int i = len-1; i>=0; i--){
+            if(Math.abs(nums[leftIndex]) > Math.abs(nums[rightIndex])){
+                result[i] = nums[leftIndex] * nums[leftIndex];
+                leftIndex++;
+            } else {
+                result[i] = nums[rightIndex] * nums[rightIndex];
+                rightIndex--;
+            }
         }
-        
-        Arrays.sort(nums);
-        return nums;
-        
+        return result;
+
     }
 }
