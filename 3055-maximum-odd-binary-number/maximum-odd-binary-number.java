52383765ffd8c76db2e2e58e8780af92b5037941
Shruti Varade <37963866+shrutivarade@users.noreply.github.com>
2024-03-09 20:28:02 -0500

    Time: 22 ms (6.38%) | Memory: 44.9 MB (5.44%) - LeetSync

--- 3055-maximum-odd-binary-number/maximum-odd-binary-number.java
+++ 3055-maximum-odd-binary-number/maximum-odd-binary-number.java
@@ -5,30 +5,30 @@ class Solution {
         // The binary representation of an odd number contains '1' in the least significant place.
 
         // Soluton 1:
-        // return Arrays.stream(s.split("")).sorted(Comparator.reverseOrder()).skip(1).collect(Collectors.joining()) + "1";
+        return Arrays.stream(s.split("")).sorted(Comparator.reverseOrder()).skip(1).collect(Collectors.joining()) + "1";
 
 
         // Solution 2:
-        int count_1 = 0, count_0 = 0, i = 0;
-        for ( char ch : s.toCharArray()){
-            if(ch=='1'){
-                count_1++;
-            }
-            else{
-                count_0++;
-            }
-        }
+        // int count_1 = 0, count_0 = 0, i = 0;
+        // for ( char ch : s.toCharArray()){
+        //     if(ch=='1'){
+        //         count_1++;
+        //     }
+        //     else{
+        //         count_0++;
+        //     }
+        // }
 
-        char[] result = new char[s.length()];
-        while(i < count_1 - 1) {
-            result[i++] = '1';
-        }        
-        while(i < count_1 - 1 + count_0) {
-            result[i++] = '0';
-        }
-        result[i] = '1';
+        // char[] result = new char[s.length()];
+        // while(i < count_1 - 1) {
+        //     result[i++] = '1';
+        // }        
+        // while(i < count_1 - 1 + count_0) {
+        //     result[i++] = '0';
+        // }
+        // result[i] = '1';
         
-        return new String(result);
+        // return new String(result);
 
     }
 }
