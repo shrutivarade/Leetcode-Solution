23a63d7a8306d64f52da94650f5dd2b04c853c4f
Shruti Varade <37963866+shrutivarade@users.noreply.github.com>
2024-03-09 19:42:13 -0500

    Time: 19 ms (64.38%) | Memory: 56.8 MB (72.04%) - LeetSync

--- 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.java
+++ 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.java
@@ -1,22 +1,48 @@
 class Solution {
     public int maxOperations(int[] nums, int k) {
-        Map<Integer, Integer> map = new HashMap<>();
+        // Method 3 : using Hashmap and 1 for loop
 
+        // Map<Integer, Integer> map = new HashMap<>();
+
+        // int count = 0;
+
+        // for ( int i = 0; i<nums.length; i++){
+
+        //     int result = k - nums[i];
+
+        //     if( map.containsKey(result) ){
+        //         count++;
+        //         if(map.get(result) == 1) map.remove(result);
+        //         else{
+        //             map.put(result, map.get(result)-1);
+        //         }
+        //     }
+        //     else{
+        //         map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
+        //     }
+        // }
+
+        // return count;
+
+        // method 2: using Arrays.sort() and 1 while loop:
+
+        Arrays.sort(nums);
+
+        int left = 0;
+        int right = nums.length - 1;
         int count = 0;
 
-        for ( int i = 0; i<nums.length; i++){
+        while (left < right) {
+            int sum = nums[left] + nums[right];
 
-            int result = k - nums[i];
-
-            if( map.containsKey(result) ){
+            if (sum == k) {
                 count++;
-                if(map.get(result) == 1) map.remove(result);
-                else{
-                    map.put(result, map.get(result)-1);
-                }
-            }
-            else{
-                map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
+                left++;
+                right--;
+            } else if (sum < k) {
+                left++;
+            } else {
+                right--;
             }
         }
 
