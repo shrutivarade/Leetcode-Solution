77e36291ede80e7279a40701119203f4251da3a4
Shruti Varade <37963866+shrutivarade@users.noreply.github.com>
2024-03-30 19:28:16 -0400

    Time: 6 ms (28.02%) | Memory: 42.9 MB (6.67%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.java
+++ 1894-merge-strings-alternately/merge-strings-alternately.java
@@ -1,9 +1,9 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
 
-        StringBuilder result = new StringBuilder();
+        // StringBuilder result = new StringBuilder();
+        
         // int i = 0;
-
         // while (i < word1.length() || i < word2.length()){
         //     if(i < word1.length()){
         //         result.append(word1.charAt(i));
@@ -14,17 +14,29 @@ class Solution {
         //     i++;
         // }
 
-        for (int i =0; i < word1.length() || i < word2.length(); i++){
-            if(i < word1.length()){
-                result.append(word1.charAt(i));
+        // for (int i =0; i < word1.length() || i < word2.length(); i++){
+        //     if(i < word1.length()){
+        //         result.append(word1.charAt(i));
+        //     }
+        //     if(i < word2.length()){
+        //         result.append(word2.charAt(i));
+        //     }
+        // }
+        // return result.toString();
+
+
+        String result = "";
+        int maxLength = Math.max(word1.length(), word2.length());
+        for (int i = 0; i < maxLength; i++) {
+            if (i < word1.length()) {
+                result += word1.charAt(i);
             }
-            if(i < word2.length()){
-                result.append(word2.charAt(i));
+            if (i < word2.length()) {
+                result += word2.charAt(i);
             }
-            // i++;
         }
-        
-        return result.toString();
+        return result;
 
     }
 }
+
