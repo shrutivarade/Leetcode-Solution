2a256226277eb2daeab9aa0dcdb2a51c7cc1632d
Shruti Varade <37963866+shrutivarade@users.noreply.github.com>
2024-03-30 22:15:45 -0400

    Time: 18 ms (93.43%) | Memory: 57.4 MB (39.14%) - LeetSync

--- 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.java
+++ 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.java
@@ -2,50 +2,50 @@ class Solution {
     public int maxOperations(int[] nums, int k) {
         // Method 3 : using Hashmap and 1 for loop:  O(n)
 
-        Map<Integer, Integer> map = new HashMap<>();
+        // Map<Integer, Integer> map = new HashMap<>();
 
-        int count = 0;
+        // int count = 0;
 
-        for ( int i = 0; i<nums.length; i++){
+        // for ( int i = 0; i<nums.length; i++){
 
-            int result = k - nums[i];
+        //     int result = k - nums[i];
 
-            if( map.containsKey(result) ){
-                count++;
-                if(map.get(result) == 1) map.remove(result);
-                else{
-                    map.put(result, map.get(result)-1);
-                }
-            }
-            else{
-                map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
-            }
-        }
+        //     if( map.containsKey(result) ){
+        //         count++;
+        //         if(map.get(result) == 1) map.remove(result);
+        //         else{
+        //             map.put(result, map.get(result)-1);
+        //         }
+        //     }
+        //     else{
+        //         map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
+        //     }
+        // }
 
-        return count;
+        // return count;
 
         // method 2: using Arrays.sort() and 1 while loop: O(n logn)
 
-        // Arrays.sort(nums);
+        Arrays.sort(nums);
 
-        // int left = 0;
-        // int right = nums.length - 1;
-        // int count = 0;
+        int left = 0;
+        int right = nums.length - 1;
+        int count = 0;
 
-        // while (left < right) {
-        //     int sum = nums[left] + nums[right];
+        while (left < right) {
+            int sum = nums[left] + nums[right];
 
-        //     if (sum == k) {
-        //         count++;
-        //         left++;
-        //         right--;
-        //     } else if (sum < k) {
-        //         left++;
-        //     } else {
-        //         right--;
-        //     }
-        // }
-        // return count;
+            if (sum == k) {
+                count++;
+                left++;
+                right--;
+            } else if (sum < k) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return count;
 
         // Method 1 Brute Force and setting -1 for values at are visited : O(n^2)
 
