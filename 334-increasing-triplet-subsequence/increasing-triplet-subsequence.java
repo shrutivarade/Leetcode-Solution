5f6e025f0afa9a382a7f0367147eba980f86c224
Shruti Varade <37963866+shrutivarade@users.noreply.github.com>
2024-03-30 21:12:05 -0400

    Time: 2 ms (93.80%) | Memory: 133.6 MB (75.99%) - LeetSync

--- 334-increasing-triplet-subsequence/increasing-triplet-subsequence.java
+++ 334-increasing-triplet-subsequence/increasing-triplet-subsequence.java
@@ -2,41 +2,41 @@ class Solution {
     public boolean increasingTriplet(int[] nums) {
 
         // Greedy algorithms make decisions based on the current best option without considering future consequences.
-        // int num1 = nums[0], num2 = Integer.MAX_VALUE;
-        // for(int n:nums){
-        //     if(n <= num1){
-        //         num1 = n;
-        //     }
-        //     else if(n <= num2){
-        //         num2 = n;
-        //     }
-        //     else{
-        //         return true;
-        //     }
-        // }
-        // return false;
-
-        //using leftmin[] and rightmax[] array to compare with the current element:
-        int n = nums.length;
-        if (n < 3){
-            return false;
-        }
-        int[] leftmin = new int[n];
-        leftmin[0] = nums[0];
-        for(int i=1; i<nums.length; i++){
-            leftmin[i] = Math.min(leftmin[i-1], nums[i]);
-        }
-        int[] rightmax = new int[n];
-        rightmax[n-1] = nums[n-1];
-        for(int i=n-2; i>=0; i--){
-            rightmax[i] = Math.max(rightmax[i+1], nums[i]);
-        }
-        for(int i=0; i<n; i++){
-            if(leftmin[i]<nums[i] && nums[i]<rightmax[i]){
+        int num1 = nums[0], num2 = Integer.MAX_VALUE;
+        for(int n:nums){
+            if(n <= num1){
+                num1 = n;
+            }
+            else if(n <= num2){
+                num2 = n;
+            }
+            else{
                 return true;
             }
         }
         return false;
+
+        //using leftmin[] and rightmax[] array to compare with the current element:
+        // int n = nums.length;
+        // if (n < 3){
+        //     return false;
+        // }
+        // int[] leftmin = new int[n];
+        // leftmin[0] = nums[0];
+        // for(int i=1; i<nums.length; i++){
+        //     leftmin[i] = Math.min(leftmin[i-1], nums[i]);
+        // }
+        // int[] rightmax = new int[n];
+        // rightmax[n-1] = nums[n-1];
+        // for(int i=n-2; i>=0; i--){
+        //     rightmax[i] = Math.max(rightmax[i+1], nums[i]);
+        // }
+        // for(int i=0; i<n; i++){
+        //     if(leftmin[i]<nums[i] && nums[i]<rightmax[i]){
+        //         return true;
+        //     }
+        // }
+        // return false;
 
         // Brute Force:
         // for (int i=0; i<nums.length; i++){
