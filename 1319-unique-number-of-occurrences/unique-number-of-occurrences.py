168f409dca11f8c9feb63dfa9369438a780c7675
Shruti Varade <37963866+shrutivarade@users.noreply.github.com>
2024-04-02 19:43:58 -0400

    Time: 39 ms (72.18%) | Memory: 16.6 MB (98.22%) - LeetSync

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.py
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.py
@@ -2,11 +2,20 @@ class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
 
         map = {}
+
         for i in arr:
-            if i in map.keys():
-                map.update({i:map.get(i)+1})
+            if i in map:
+                map[i] = map[i]+1
             else:
-                map.setdefault(i,1)
+                map[i] = 1
+
+        # for i in arr:
+        #     if i in map.keys():
+        #         map.update({i:map.get(i)+1})
+        #     else:
+        #         map.setdefault(i,1)
+
+
         if(len(map.values()) != len(set(map.values()))):
             return False
         else:
