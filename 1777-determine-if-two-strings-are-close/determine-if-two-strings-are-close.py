41a2b157690539ec95b55d03bb801d591124936c
Shruti Varade <37963866+shrutivarade@users.noreply.github.com>
2024-04-04 17:36:18 -0400

    Time: 63 ms (99.86%) | Memory: 17.8 MB (16.73%) - LeetSync

--- 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.py
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.py
@@ -6,23 +6,22 @@ class Solution:
         #     return False
         
         # # create a dictionary to track count of characters in both the strings.
-
         # k1 = Counter(word1)
         # k2 = Counter(word2)
-        
 
         # # frequency of all values
         # v1 = Counter(list(k1.values()))
         # v2 = Counter(list(k2.values()))
         # if(v1 != v2):
         #     return False
-        
         # return True
 
 
         # if any characters are different immediately return False
         unique1, unique2 = set(word1), set(word2)
-        if unique1 != unique2: return False
+        if unique1 != unique2: 
+            return False
+
 
         freq1 = sorted([word1.count(i) for i in unique1])
         freq2 = sorted([word2.count(i) for i in unique2])
