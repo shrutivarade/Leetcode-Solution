2f85b481a2c4cc798789db4ce7432271cd1527ca
Shruti Varade <37963866+shrutivarade@users.noreply.github.com>
2024-04-04 17:24:37 -0400

    Time: 126 ms (83.72%) | Memory: 17.8 MB (16.73%) - LeetSync

--- 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.py
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.py
@@ -1,19 +1,21 @@
 class Solution:
     def closeStrings(self, word1: str, word2: str) -> bool:
 
-        # count the frequency of all the characters in word1 and word2 and create a hashmap/dict (key:value)
-        c1 = Counter(word1)
-        c2 = Counter(word2)
+        # unique characters from both the strings should be same.
+        if(set(word1)!=set(word2)):
+            return False
+        
+        # create a dictionary to track count of characters in both the strings.
+        # frequency of all keys.
+        k1 = Counter(word1)
+        k2 = Counter(word2)
+        # if(k1 != k2):
+        #     return False
 
-        #  count the frequency of all the values in c1 and c2
-        # n1 = Counter([v for v in c1.values()])
-        # n2 = Counter([v for v in c2.values()])
-
-        n1 = Counter(list(c1.values()))
-        n2 = Counter(list(c2.values()))
-
-        one  = True if c1==c2 else False
-        two  = True if n1==n2 else False
-        three  = True if set(word1) == set(word2) else False
-
-        return one or (two and three)
+        # frequency of all values
+        v1 = Counter(list(k1.values()))
+        v2 = Counter(list(k2.values()))
+        if(v1 != v2):
+            return False
+        
+        return True
