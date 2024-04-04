c13074ec7d76db8a36439b3fbfb6a2a869c2b4e7
Shruti Varade <37963866+shrutivarade@users.noreply.github.com>
2024-04-04 17:26:37 -0400

    Time: 63 ms (99.86%) | Memory: 17.7 MB (32.98%) - LeetSync

--- 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.py
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.py
@@ -1,21 +1,30 @@
 class Solution:
     def closeStrings(self, word1: str, word2: str) -> bool:
 
-        # unique characters from both the strings should be same.
-        if(set(word1)!=set(word2)):
-            return False
-        
-        # create a dictionary to track count of characters in both the strings.
-        # frequency of all keys.
-        k1 = Counter(word1)
-        k2 = Counter(word2)
-        # if(k1 != k2):
+        # # unique characters from both the strings should be same.
+        # if(set(word1)!=set(word2)):
         #     return False
-
-        # frequency of all values
-        v1 = Counter(list(k1.values()))
-        v2 = Counter(list(k2.values()))
-        if(v1 != v2):
-            return False
         
-        return True
+        # # create a dictionary to track count of characters in both the strings.
+
+        # k1 = Counter(word1)
+        # k2 = Counter(word2)
+        
+
+        # # frequency of all values
+        # v1 = Counter(list(k1.values()))
+        # v2 = Counter(list(k2.values()))
+        # if(v1 != v2):
+        #     return False
+        
+        # return True
+
+
+        # if any characters are different immediately return False
+        unique1, unique2 = set(word1), set(word2)
+        if unique1 != unique2: return False
+
+        freq1 = sorted([word1.count(i) for i in unique1])
+        freq2 = sorted([word2.count(i) for i in unique2])
+
+        return freq1 == freq2
