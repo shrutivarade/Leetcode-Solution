aa2d50b8b782924b89341b2bfb051f34d4fc9a0f
Shruti Varade <37963866+shrutivarade@users.noreply.github.com>
2024-04-04 23:04:10 -0400

    Time: 8 ms (91.57%) | Memory: 50.7 MB (47.81%) - LeetSync

--- 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.java
+++ 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.java
@@ -1,30 +1,46 @@
+// class Solution {
+//     public int equalPairs(int[][] grid) {
+
+//         //create a hm to keep track of number of times a given row occurs
+//         HashMap<List<Integer>, Integer> map = new HashMap<>();
+//         int n = grid.length; // number odf rows
+
+//         // iterate over each row, create a arraylist for each row and put it as a key in hm
+//         for (int i=0; i<n; i++){
+//             List<Integer> row = new ArrayList<>();
+//             for(int j=0; j<grid[0].length;j++){
+//                 row.add(grid[i][j]);
+//             }
+//             map.put(row, map.getOrDefault(row,0)+1);
+//         }
+
+//         // iterate over each column, create a arraylist for each column and check if it is present in hm.
+//         int count = 0;
+//         for (int i=0; i<n; i++){
+//             List<Integer> col = new ArrayList<>();
+//             for(int j=0; j<grid[0].length;j++){
+//                 col.add(grid[j][i]);
+//             }
+//             if(map.containsKey(col)){
+//                 count+=map.get(col);
+//             }
+//         }
+//         return count;
+//     }
+// }
+
 class Solution {
     public int equalPairs(int[][] grid) {
-
-        //create a hm to keep track of number of times a given row occurs
-        HashMap<List<Integer>, Integer> map = new HashMap<>();
-        int n = grid.length; // number odf rows
-
-        // iterate over each row, create a arraylist for each row and put it as a key in hm
-        for (int i=0; i<n; i++){
-            List<Integer> row = new ArrayList<>();
-            for(int j=0; j<grid[0].length;j++){
-                row.add(grid[i][j]);
+        int pairs = 0, len = grid.length;
+        for(int i=0;i<len;i++) {
+            int[] temp = new int[len];
+            for(int j=0;j<len;j++) {
+                temp[j] = grid[j][i];
             }
-            map.put(row, map.getOrDefault(row,0)+1);
-        }
-
-        // iterate over each column, create a arraylist for each column and check if it is present in hm.
-        int count = 0;
-        for (int i=0; i<n; i++){
-            List<Integer> col = new ArrayList<>();
-            for(int j=0; j<grid[0].length;j++){
-                col.add(grid[j][i]);
-            }
-            if(map.containsKey(col)){
-                count+=map.get(col);
+            for(int k=0;k<len;k++) {
+                pairs += Arrays.equals(temp, grid[k]) ? 1 : 0;
             }
         }
-        return count;
+        return pairs;  
     }
-}
+}   
