b7632880aeb5c724bc2a871527ea1f48470fb638
Shruti Varade <37963866+shrutivarade@users.noreply.github.com>
2024-10-10 13:42:08 -0400

    Time: 65 ms (43.74%) | Memory: 17.8 MB (37.16%) - LeetSync

---
+++ 1-two-sum/two-sum.py
@@ -0,0 +1,17 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        
+        dict = {}
+
+        for i, num in enumerate(nums):
+            if target - num in dict:
+                return [dict[target - num], i]
+            dict[num] = i
+
+        return []
+
