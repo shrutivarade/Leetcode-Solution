4d2a78d2e21712d4e67046fe46e5993b90e31f82
Shruti Varade <37963866+shrutivarade@users.noreply.github.com>
2024-10-11 14:08:31 -0400

    Time: 56 ms (75.44%) | Memory: 17.8 MB (37.45%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -6,18 +6,17 @@ class Solution:
         :rtype: List[int]
         """
         # Brute force: O(n^2)
-        for i in range(0, len(nums)):
-            for j in range(i+1, len(nums)):
-                if target == nums[i]+nums[j]:
-                    return [i,j]
-        return []
-
-        # Optimal: O(n)
-        # dict = {}
-        # for i in range(len(nums)):
-        #     if target - nums[i] in dict:
-        #         return [dict[target - nums[i]], i]
-        #     dict[nums[i]] = i
-
+        # for i in range(0, len(nums)):
+        #     for j in range(i+1, len(nums)):
+        #         if target == nums[i]+nums[j]:
+        #             return [i,j]
         # return []
 
+        # Optimal: O(n)
+        dict = {}
+        for i in range(len(nums)):
+            if target - nums[i] in dict:
+                return [dict[target - nums[i]], i]
+            dict[nums[i]] = i
+
+        return []
