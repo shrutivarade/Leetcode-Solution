c978c9aecb956d4f8818da9888f767c6fb933766
Shruti Varade <37963866+shrutivarade@users.noreply.github.com>
2024-10-12 15:30:49 -0400

    Time: 0 ms (100.00%) | Memory: 40.1 MB (80.28%) - LeetSync

--- 371-sum-of-two-integers/sum-of-two-integers.java
+++ 371-sum-of-two-integers/sum-of-two-integers.java
@@ -2,8 +2,8 @@ class Solution {
     public int getSum(int a, int b) {
 
         while (b!=0){
-            int tmp = (a&b)<<1;
-            a = a^b;
+            int tmp = (a&b)<<1; // logical AND and shift all bits to left by 1
+            a = a^b; // logical XOR
             b = tmp;
         }
         return a;
