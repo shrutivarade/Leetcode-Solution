0865d1858a741d029599d4c351f3a273295f1733
Shruti Varade <37963866+shrutivarade@users.noreply.github.com>
2024-10-13 16:13:22 -0400

    Time: 24 ms (99.98%) | Memory: 17 MB (57.83%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,26 +1,36 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         
-        # Convert strings s and t into lists
-        a = []
-        for i in s:
-            a.append(i)
-        b = []
-        for i in t:
-            b.append(i)
+#         # Convert strings s and t into lists
+#         a = []
+#         for i in s:
+#             a.append(i)
+#         b = []
+#         for i in t:
+#             b.append(i)
 
-        # Sort the lists in ascending order
-        a.sort()
-        b.sort()
+#         # Sort the lists in ascending order
+#         a.sort()
+#         b.sort()
 
-        # Compare the sorted lists
-        if a == b:
-            return True
-        else:
-            return False
+#         # Compare the sorted lists
+#         if a == b:
+#             return True
+#         else:
+#             return False
 
-# Time complexity analysis:
-# - Converting strings `s` and `t` into lists using a loop takes O(n) time, where n is the length of the longer string between `s` and `t`.
-# - Sorting the lists `a` and `b` using the `sort()` method takes O(n log n) time, as it uses an efficient sorting algorithm like Timsort or Quicksort.
-# - Comparing the sorted lists `a` and `b` using the `==` operator takes O(n) time, as it needs to compare each element of the lists.
-# - Therefore, the overall time complexity of this code is dominated by the sorting step, which is O(n log n), where n is the length of the longer string between `s` and `t`.
+# # Time complexity analysis:
+# # - Converting strings `s` and `t` into lists using a loop takes O(n) time, where n is the length of the longer string between `s` and `t`.
+# # - Sorting the lists `a` and `b` using the `sort()` method takes O(n log n) time, as it uses an efficient sorting algorithm like Timsort or Quicksort.
+# # - Comparing the sorted lists `a` and `b` using the `==` operator takes O(n) time, as it needs to compare each element of the lists.
+# # - Therefore, the overall time complexity of this code is dominated by the sorting step, which is O(n log n), where n is the length of the longer string between `s` and `t`.
+
+        mapS = Counter(s)
+        mapT = Counter(t)
+
+        return mapS == mapT
+
+        # if len(s)==len(t) and len(set(s)) == len(set(t)) and set(s)==set(t):
+        #     return True
+        # else:
+        #     return False
