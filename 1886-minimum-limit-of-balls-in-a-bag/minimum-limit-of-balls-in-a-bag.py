3cc4b1e7404971e0087caed1004e938fc441f4d6
Shruti Varade <37963866+shrutivarade@users.noreply.github.com>
2024-12-07 17:34:19 -0500

    Time: 564 ms (79.50%) | Memory: 29.1 MB (36.56%) - LeetSync

---
+++ 1886-minimum-limit-of-balls-in-a-bag/minimum-limit-of-balls-in-a-bag.py
@@ -0,0 +1,42 @@
+class Solution:
+    def minimumSize(self, nums: List[int], maxOperations: int) -> int:
+        # return 0
+
+        # def greatest_factor_excluding_self(number):
+        #     for i in range(number // 2, 1, -1):
+        #         if number % i == 0:
+        #             return i 
+        #     return float('inf') 
+
+        # result = float('inf')
+        # for n in nums:
+        #     result = min(result, greatest_factor_excluding_self(n))
+   
+        # return result
+
+        # smallest = int (sum(nums) / (len(nums) + maxOperations))
+        # while maxOperations > 0:
+        #     maxNum = max(nums)
+        #     nums.remove(maxNum)
+        #     nums.append(maxNum-smallest)
+        #     nums.append(smallest)
+        #     maxOperations -= 1
+        # print(nums)
+        # return max(nums)
+
+        low, high = 1, max(nums) # min and max posible bags
+        while low < high:
+            mid = (low + high) // 2
+            if sum((n - 1) // mid for n in nums) <= maxOperations: high = mid
+            else: low = mid + 1
+        return high
+        
+
+
+        
+
+
+
+
+
+
