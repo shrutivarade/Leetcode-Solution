70439e5def9cb217b48e8f33fe7f38a95b9bd4d2
Shruti Varade <37963866+shrutivarade@users.noreply.github.com>
2024-12-13 18:58:07 -0500

    Time: 43 ms (99.30%) | Memory: 28.9 MB (98.75%) - LeetSync

---
+++ 2695-find-score-of-an-array-after-marking-all-elements/find-score-of-an-array-after-marking-all-elements.py
@@ -0,0 +1,71 @@
+# class Solution:
+#     def findScore(self, nums: List[int]) -> int:
+#         score = 0
+#         while nums:
+#             minVal = min(nums)
+#             idx = nums.index(minVal)
+#             score += nums[idx]
+
+#             if(nums.index(min(nums))>0 and nums.index(min(nums))<len(nums)-1):
+#                 nums.remove(nums[idx-1])
+#                 nums.remove(nums[idx-1])
+#                 nums.remove(nums[idx-1])
+#                 continue
+#             elif (nums.index(min(nums))>0 and not nums.index(min(nums))<len(nums)-1):
+#                 nums.remove(nums[idx-1])
+#                 nums.remove(nums[idx-1])
+#                 continue
+#             elif (not nums.index(min(nums))>0 and nums.index(min(nums))<len(nums)-1):
+#                 nums.remove(nums[idx+1])
+#                 nums.remove(nums[idx])
+#                 continue
+
+#             if(len(nums)==1):
+#                 nums.remove(nums[idx])
+#                 continue
+#             elif(len(nums)==2 and nums.index(min(nums))>0):
+#                 nums.remove(nums[idx-1])
+#                 nums.remove(nums[idx-1])
+#                 continue
+#             elif(len(nums)==2 and nums.index(min(nums))<len(nums)-1):
+#                 nums.remove(nums[idx+1])
+#                 nums.remove(nums[idx])
+#                 continue
+        
+
+            
+#         return score
+
+
+
+class Solution:
+    def findScore(self, nums: List[int]) -> int:
+        score = 0
+        n = len(nums)
+        q = deque()
+
+        # Traverse through the array
+        for i in range(n):
+            # If queue is not empty and the current number is greater than or equal to the last in queue
+            if q and nums[i] >= q[-1]:
+                skip = False
+                # Process the elements in the queue
+                while q:
+                    add = q.pop()
+                    if not skip:
+                        score += add
+                    skip = not skip
+                continue
+
+            # Add current element to the queue
+            q.append(nums[i])
+
+        # Final processing of remaining elements in the queue
+        skip = False
+        while q:
+            add = q.pop()
+            if not skip:
+                score += add
+            skip = not skip
+
+        return score
