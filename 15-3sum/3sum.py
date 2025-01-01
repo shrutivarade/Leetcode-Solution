6846eea88daa63047bd0f7de123e834fbed0080c
Shruti Varade <37963866+shrutivarade@users.noreply.github.com>
2025-01-01 15:39:42 -0500

    Time: 570 ms (63.05%) | Memory: 20.6 MB (27.46%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,13 +1,14 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         # bruteforce: O(n^3) 3 for loops was getting results with duplicates
-        
+        [-1,-1,-1,0,2,2]
         nums.sort()
         result = []
         #  sorted: [-4, -1, -1, 0, 1, 2]
         for i in range(0, len(nums)-1):
             if i>0 and nums[i]==nums[i-1]: # for i^th pointer, outer loop
                 continue
+                
             l, r = i + 1, len(nums)-1
             while l<r:
                 sum = nums[i] + nums[l] + nums[r]
@@ -23,3 +24,30 @@ class Solution:
 
         return result
  
+
+
+
+        # extended 2 sum, works only for easy test cases
+        # result = []
+        # for i in range(len(nums)):
+        #     target = 0-nums[i]
+        #     arr = []
+        #     for j in range(i+1, len(nums)):
+        #         if(target-nums[j] in arr):
+        #             if(sorted([nums[i], nums[j], target-nums[j]]) not in result):
+        #                 result.append(sorted([nums[i], nums[j], target-nums[j]]))
+        #         else:
+        #             arr.append(nums[j])
+        # return result
+
+
+
+
+
+
+
+
+
+
+
+
