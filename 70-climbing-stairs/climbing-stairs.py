5eb129d5d0415b991b029f1e8a1cdf67f68703e9
Shruti Varade <37963866+shrutivarade@users.noreply.github.com>
2025-01-02 16:00:25 -0500

    Time: 0 ms (100.00%) | Memory: 17.6 MB (24.79%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,6 +1,34 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
 
+        dp = [0] * (n+1)
+
+        dp[n] = 1
+        dp[n-1] = 1
+
+        for i in range(n-2, -1, -1):
+            dp[i] = dp[i+1] + dp[i+2]
+
+        return dp[0]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         #its basically a fibonacci series
         # 2-2
         # 3-3
@@ -9,17 +37,18 @@ class Solution:
         # 6-13
         # 7-21
 
-        if n < 0:
-            return -1 # Invalid input
-        elif n == 0:
-            return 0 # Base case: n is 0, return 0
-        elif n == 1:
-            return 1 # Base case: n is 1, return 1
-        else:
-            a = 0
-            b = 1
-            for i in range(2, n+2):
-                c = a + b
-                a = b
-                b = c
-            return b
+        # if n < 0:
+        #     return -1 # Invalid input
+        # elif n == 0:
+        #     return 0 # Base case: n is 0, return 0
+        # elif n == 1:
+        #     return 1 # Base case: n is 1, return 1
+        # else:
+        #     # to understand this solution try dry run for 1 stairs, 2 stairs, 3 stairs and 4th stair and see the pattern. 
+        #     a = 0
+        #     b = 1
+        #     for i in range(2, n+2):
+        #         c = a + b
+        #         a = b
+        #         b = c
+        #     return b
