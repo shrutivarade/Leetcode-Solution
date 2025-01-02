9f827ea0eb855b0c573cdf5e8a6ea7d1fa5d30cb
Shruti Varade <37963866+shrutivarade@users.noreply.github.com>
2025-01-02 15:27:42 -0500

    Time: 1303 ms (9.78%) | Memory: 18.8 MB (11.04%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -8,12 +8,16 @@ class Solution:
         #     if(i not in count):
         #         return i
 
-        # Space O(1), Time O(n), using addition
-        res = len(nums)
+        # Space O(1), Time O(n), using addition, sum([0,1,2,3]) - sum([3,0,1]) = 2
+        # res = len(nums)
+        # for i in range(len(nums)):
+        #     res += (i-nums[i])
+        # return res
 
-        for i in range(len(nums)):
-            res += (i-nums[i])
-        return res
+
+        for i in range(len(nums)+1):
+            if i not in nums:
+                return i
 
 
 
