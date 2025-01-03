ad97d2f596a438ccc63a16efca716470ca1a9dda
Shruti Varade <37963866+shrutivarade@users.noreply.github.com>
2025-01-03 16:53:07 -0500

    Time: 0 ms (100.00%) | Memory: 17.9 MB (9.33%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,8 +1,48 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        # 1,2,3,1,0
-        nums.append(0)
-        for i in range(len(nums)-4, -1, -1):
-            nums[i] = max(nums[i]+nums[i+2], nums[i]+nums[i+3])
-        return max(nums[0], nums[1])
+
+
+        dp = [0] * len(nums)
+        dp[len(nums)-1] = nums[len(nums)-1]
+        dp[len(nums)-2] = nums[len(nums)-2]
+
+        for i in range(len(nums)-3, -1, -1):
+            dp[i] = nums[i] + max(dp[i+2:])
+        return max(dp)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # # 1,2,3,1,0
+        # nums.append(0)
+        # for i in range(len(nums)-4, -1, -1):
+        #     nums[i] = max(nums[i]+nums[i+2], nums[i]+nums[i+3])
+        # return max(nums[0], nums[1])
         
