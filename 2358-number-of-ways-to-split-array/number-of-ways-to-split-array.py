98cda4078b0325c5f463ea02c374ecd782cdbcc8
Shruti Varade <37963866+shrutivarade@users.noreply.github.com>
2025-01-03 10:55:17 -0500

    Time: 36 ms (98.89%) | Memory: 32.4 MB (58.91%) - LeetSync

--- 2358-number-of-ways-to-split-array/number-of-ways-to-split-array.py
+++ 2358-number-of-ways-to-split-array/number-of-ways-to-split-array.py
@@ -10,10 +10,11 @@ class Solution:
         #         count+=1
         # return count
 
-        n=len(nums)
-        Sum=sum(nums)
-        acc, cnt=0, 0
-        for i in range(n-1):
-            acc+=nums[i]
-            cnt+=(2*acc>=Sum)
-        return cnt
+        left = 0
+        right = (sum(nums) + 1) // 2
+        res = 0
+        for i in range(len(nums) - 1):
+            left += nums[i]
+            if left >= right:
+                res += 1
+        return res
