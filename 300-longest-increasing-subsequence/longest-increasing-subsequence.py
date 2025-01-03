814dbe42dac98c954c48f304c73738b3fd47829f
Shruti Varade <37963866+shrutivarade@users.noreply.github.com>
2025-01-03 15:38:42 -0500

    Time: 909 ms (70.65%) | Memory: 18.2 MB (16.21%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.py
@@ -1,6 +1,15 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
 
+        # Start from last element
+        # last elmet itself is an LIS
+        # then iterate backward using i and then forward from i->n using j
+        # update dp tabel only if nums[j]>nums[i] 
+        # and LIS[i]<=LIS[j] to avoid overwrite the value that is updated once,
+        # eg. 3,7,101,18, for 3 the dp value should me 3 consisting of 3-7-101 or 3-7-18
+        #  but the i->3 and j->101 the value is unnecessarily modified.
+
+
 
         LIS = [1] * len(nums)
         for i in range(len(nums)-1,-1,-1):
@@ -9,40 +18,3 @@ class Solution:
                     LIS[i] = 1+LIS[j]
         return max(LIS)
 
-
-
-
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        # n = len(nums)
-
-        # lis = [1] * n
-
-        # for i in range(1,n):
-        #     for j in range(0,i):
-        #         if((nums[i] > nums[j]) and lis[i] <=lis[j]):
-        #             lis[i] = 1+lis[j]
-        # return max(lis)
