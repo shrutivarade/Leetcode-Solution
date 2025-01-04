faa94c0b2235a4df2317b41d19fce163ae6f5423
Shruti Varade <37963866+shrutivarade@users.noreply.github.com>
2025-01-04 10:52:07 -0500

    Time: 33 ms (94.09%) | Memory: 18.3 MB (9.49%) - LeetSync

--- 133-clone-graph/clone-graph.py
+++ 133-clone-graph/clone-graph.py
@@ -9,19 +9,33 @@ class Node:
 from typing import Optional
 class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
-        if not node: return node
+        # if not node: return node
         
-        q = deque([node])
-        clones  = {node.val: Node(node.val, [])}
-        while q:
-            cur = q.popleft() 
-            cur_clone = clones[cur.val]            
+        # q = deque([node])
+        # clones  = {node.val: Node(node.val, [])}
+        # while q:
+        #     cur = q.popleft() 
+        #     cur_clone = clones[cur.val]            
 
-            for ngbr in cur.neighbors:
-                if ngbr.val not in clones:
-                    clones[ngbr.val] = Node(ngbr.val, [])
-                    q.append(ngbr)
+        #     for ngbr in cur.neighbors:
+        #         if ngbr.val not in clones:
+        #             clones[ngbr.val] = Node(ngbr.val, [])
+        #             q.append(ngbr)
                     
-                cur_clone.neighbors.append(clones[ngbr.val])
+        #         cur_clone.neighbors.append(clones[ngbr.val])
                 
-        return clones[node.val]
+        # return clones[node.val]
+
+        if not node:
+            return None
+        old_to_new = {None: None}
+
+        def dfs(node):
+            if node in old_to_new:
+                return old_to_new[node]
+            old_to_new[node] = Node(node.val)
+            for nei in node.neighbors:
+                old_to_new[node].neighbors.append(dfs(nei))
+            return old_to_new[node]
+
+        return dfs(node)
