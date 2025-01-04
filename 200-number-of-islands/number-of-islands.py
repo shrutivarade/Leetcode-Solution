f45935e23e16e668437a0fe787159daa0a87f566
Shruti Varade <37963866+shrutivarade@users.noreply.github.com>
2025-01-04 18:16:23 -0500

    Time: 215 ms (98.38%) | Memory: 20 MB (51.04%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,10 +1,11 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
 
+        # DFS
         row, col = len(grid), len(grid[0])
-        res = 0
+        islands = 0
         def dfs(r,c,grid):         
-            grid[r][c] = "0"
+            grid[r][c] = "0"    # mark the visited 1's and 0's
             if r < row -1 and grid[r+1][c] == "1":
                 dfs(r+1,c,grid)
             if r > 0 and grid[r-1][c] == "1":
@@ -18,36 +19,36 @@ class Solution:
             for c in range(col):
                 if grid[r][c] == "1":
                     dfs(r,c,grid)
-                    res+=1
-        return res
+                    islands+=1
+        return islands
         
-        # if not grid:
-        #     return None
-        
-        # rows, cols = len(grid), len(grid[0])
-        # islands = 0
-        # visited = set()
 
-        # def bfs(r,c): # this function is responsible to all the connect 1's in visited set
-        #     q = collections.deque()
-        #     visited.add((r,c))
-        #     q.append((r,c))
-        #     while q:
-        #         row, col = q.popleft()
-        #         directions = [[1,0], [-1,0], [0,1], [0,-1]] # all four directions
+
+        # BFS
+        rows, cols = len(grid), len(grid[0])
+        islands = 0
+        visited = set()
+
+        def bfs(r,c): # this function is responsible to all the connect 1's in visited set
+            q = collections.deque()
+            visited.add((r,c))
+            q.append((r,c))
+            while q:
+                row, col = q.popleft()
+                directions = [[1,0], [-1,0], [0,1], [0,-1]] # all four directions
                 
-        #         for dr, dc in directions:
-        #             r = row+dr
-        #             c = col+dc
-        #             if (r in range(rows) and c in range(cols) and grid[r][c] == "1" and (r,c) not in visited):
-        #                 q.append((r,c))
-        #                 visited.add((r,c))
+                for dr, dc in directions:
+                    r = row+dr
+                    c = col+dc
+                    if (r in range(rows) and c in range(cols) and grid[r][c] == "1" and (r,c) not in visited):
+                        q.append((r,c))
+                        visited.add((r,c))
 
 
-        # for r in range(rows):
-        #     for c in range(cols):
-        #         if grid[r][c] == "1" and (r,c) not in visited:
-        #             bfs(r,c)
-        #             islands+=1
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == "1" and (r,c) not in visited:
+                    bfs(r,c)
+                    islands+=1
         
-        # return islands
+        return islands
