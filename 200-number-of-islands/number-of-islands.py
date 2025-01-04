0c918a9ce7e0670a24100b0e5e72dd734611c0b5
Shruti Varade <37963866+shrutivarade@users.noreply.github.com>
2025-01-04 18:13:52 -0500

    Time: 320 ms (8.40%) | Memory: 25.5 MB (16.67%) - LeetSync

---
+++ 200-number-of-islands/number-of-islands.py
@@ -0,0 +1,33 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+
+        if not grid:
+            return None
+        
+        rows, cols = len(grid), len(grid[0])
+        islands = 0
+        visited = set()
+
+        def bfs(r,c): # this function is responsible to all the connect 1's in visited set
+            q = collections.deque()
+            visited.add((r,c))
+            q.append((r,c))
+            while q:
+                row, col = q.popleft()
+                directions = [[1,0], [-1,0], [0,1], [0,-1]] # all four directions
+                
+                for dr, dc in directions:
+                    r = row+dr
+                    c = col+dc
+                    if (r in range(rows) and c in range(cols) and grid[r][c] == "1" and (r,c) not in visited):
+                        q.append((r,c))
+                        visited.add((r,c))
+
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == "1" and (r,c) not in visited:
+                    bfs(r,c)
+                    islands+=1
+        
+        return islands
