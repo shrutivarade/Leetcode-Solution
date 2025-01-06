e5ad81864b0ea10d0418e85b10f15db7c7400a01
Shruti Varade <37963866+shrutivarade@users.noreply.github.com>
2025-01-06 18:44:24 -0500

    Time: 3 ms (91.89%) | Memory: 21.3 MB (36.73%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -6,17 +6,20 @@ class Solution:
         :type k: int
         :rtype: List[int]
         """
-        
+        # Generic way
         # counter = Counter(nums)
         # return [num for num, _ in counter.most_common(k)]
 
-        freq_count= Counter(nums)
 
+        # Using Priority queue and min heap
+        freq_count= Counter(nums)
         min_heap=[]
 
         for num, freq in freq_count.items():
             heapq.heappush(min_heap, (freq, num))
-
             if len(min_heap)> k:
                 heapq.heappop(min_heap)
+                
         return [num for freq, num in min_heap]
+
+        # use this testcase: [1,2,2,3,3,3,4,4,4,4,5,5,5,5,5,6,6,6,6,6,6]
