1a2275811b2bdebcda61c3e19e8e6591113b17a8
Shruti Varade <37963866+shrutivarade@users.noreply.github.com>
2025-01-09 13:36:50 -0500

    Time: 1 ms (99.64%) | Memory: 55.9 MB (29.59%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,30 +1,74 @@
 
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        // Get the length of the input array
-        int length = nums.length;
 
-        // Initialize the left product array with 1
-        int[] L = new int[length];
-        L[0] = 1;
-        // Calculate the product of all elements to the left of each index
-        for (int i = 1; i < length; i++) {
-            L[i] = nums[i - 1] * L[i - 1];
+        int len = nums.length;
+
+        int[] res = new int[len];
+        for (int i = 0; i < len; i++) {
+            res[i] = 1;
         }
 
-        // Initialize the right product array with 1
-        int[] R = new int[length];
-        R[length - 1] = 1;
-        // Calculate the product of all elements to the right of each index
-        for (int i = length - 2; i >= 0; i--) {
-            R[i] = nums[i + 1] * R[i + 1];
+        int prefix = 1;
+        for ( int i=0; i<len; i++){
+            res[i] = prefix;
+            prefix *= nums[i];
         }
 
-        // Calculate the final product by multiplying the corresponding left and right products
-        int[] answer = new int[length];
-        for (int i = 0; i < length; i++) {
-            answer[i] = L[i] * R[i];
+        int postfix = 1;
+        for( int i=len-1; i>=0; i--){
+            res[i] *= postfix;
+            postfix *= nums[i];
         }
-        return answer;
+    
+        return res;
     }
 }
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    //     // Get the length of the input array
+    //     int length = nums.length;
+
+    //     // Initialize the left product array with 1
+    //     int[] L = new int[length];
+    //     L[0] = 1;
+    //     // Calculate the product of all elements to the left of each index
+    //     for (int i = 1; i < length; i++) {
+    //         L[i] = nums[i - 1] * L[i - 1];
+    //     }
+
+    //     // Initialize the right product array with 1
+    //     int[] R = new int[length];
+    //     R[length - 1] = 1;
+    //     // Calculate the product of all elements to the right of each index
+    //     for (int i = length - 2; i >= 0; i--) {
+    //         R[i] = nums[i + 1] * R[i + 1];
+    //     }
+
+    //     // Calculate the final product by multiplying the corresponding left and right products
+    //     int[] answer = new int[length];
+    //     for (int i = 0; i < length; i++) {
+    //         answer[i] = L[i] * R[i];
+    //     }
+    //     return answer;
+    // }
+
