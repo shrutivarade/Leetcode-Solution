8d389186dc4d1d9ac11f08aea0f2b6c51ce19b20
Shruti Varade <37963866+shrutivarade@users.noreply.github.com>
2025-03-01 11:44:40 -0500

    Time: 3 ms (34.56%) | Memory: 18 MB (46.47%) - LeetSync

---
+++ 2551-apply-operations-to-an-array/apply-operations-to-an-array.py
@@ -0,0 +1,26 @@
+class Solution:
+    def applyOperations(self, nums: List[int]) -> List[int]:
+
+        n = len(nums)
+        print(nums)
+        # logic to iterate over the nums array 0->n-1
+        for i in range(n-1):
+            if(nums[i]==nums[i+1]):
+                nums[i]=nums[i]*2
+                nums[i+1]=0
+        
+        print(nums)
+
+        nonZeros = []
+        for num in nums:
+            if num != 0:
+                nonZeros.append(num)
+        
+        while len(nonZeros) != n:
+            nonZeros.append(0)
+
+        return nonZeros
+
+
+
+        
