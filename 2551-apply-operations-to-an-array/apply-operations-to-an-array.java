6eb869cc87d1ffda6ba0ac883b82a0a346d2c03d
Shruti Varade <37963866+shrutivarade@users.noreply.github.com>
2025-03-01 11:52:45 -0500

    Time: 1 ms (96.37%) | Memory: 44.3 MB (12.05%) - LeetSync

---
+++ 2551-apply-operations-to-an-array/apply-operations-to-an-array.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] applyOperations(int[] nums) {
+        int n = nums.length;
+        for(int i=0; i<n-1; i++){
+            if(nums[i]==nums[i+1]){
+                nums[i] *= 2;
+                nums[i+1] = 0;
+            }
+        }
+
+        int[] nonZeros = new int[n];
+        int idx = 0;
+        for(int num : nums){
+            if(num!=0){
+                nonZeros[idx] = num;
+                idx++;
+
+            }
+        }
+
+        return nonZeros;
+    }
+}
