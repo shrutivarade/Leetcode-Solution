bd2e2bd9e4a69132761bc4b48d8791c4209c86da
Shruti Varade <37963866+shrutivarade@users.noreply.github.com>
2025-03-03 15:47:47 -0500

    Time: 22 ms (93.03%) | Memory: 35.4 MB (22.38%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.py
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.py
@@ -1,50 +1,43 @@
 class Solution:
     def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
 
-        n = len(nums)
-        i,j,l,r = 0, n-1, 0, n-1
-        res = [0] * n
-        while i<n and j>-1:
+        # n = len(nums)
+        # i,j,l,r = 0, n-1, 0, n-1
+        # res = [0] * n
+        # while i<n and j>-1:
 
-            # # Both are pivot
-            # if nums[i]==pivot and nums[j]==pivot:
-            #     i+=1
-            #     j-=1
-            #     continue
+        #     if nums[i]<pivot:
+        #         res[l] = nums[i]
+        #         l+=1
+        #     if nums[j]>pivot:
+        #         res[r] = nums[j]
+        #         r-=1
+        
+        #     i+=1
+        #     j-=1
 
-            # #  either of them is pivot
-            # if nums[i]==pivot: 
-            #     i+=1
-            #     if nums[j]>pivot:
-            #         res[r] = nums[j]
-            #         r-=1
-            #     j-=1
-            #     continue
-                
-            # if nums[j]==pivot: 
-            #     j-=1
-            #     if nums[i]<pivot:
-            #         res[l] = nums[i]
-            #         l+=1
-            #     i+=1
-            #     continue
-
-            #  None of them is pivot
-            if nums[i]<pivot:
-                res[l] = nums[i]
-                l+=1
-            if nums[j]>pivot:
-                res[r] = nums[j]
-                r-=1
-                
-            i+=1
-            j-=1
-        j+=1
-        while j<n:
-            if nums[j]==pivot: 
-                res[l] = pivot 
-                l+=1
-            j+=1
+        # j+=1
+        # while j<n:
+        #     if nums[j]==pivot: 
+        #         res[l] = pivot 
+        #         l+=1
+        #     j+=1
 
         
+        # return res
+
+        small = []
+        big = []
+        c = 0
+        for i in nums:
+            if i<pivot:
+                small.append(i)
+            elif i>pivot:
+                big.append(i)
+            elif i==pivot:
+                c+=1
+        res = []
+        res += small
+        res += [pivot]*c
+        res += big
         return res
