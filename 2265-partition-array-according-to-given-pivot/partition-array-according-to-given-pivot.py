5054cc40973583985efb7f82d5c9193b08bb086a
Shruti Varade <37963866+shrutivarade@users.noreply.github.com>
2025-03-03 15:38:01 -0500

    Time: 101 ms (6.19%) | Memory: 35 MB (23.06%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.py
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.py
@@ -6,28 +6,28 @@ class Solution:
         res = [0] * n
         while i<n and j>-1:
 
-            # Both are pivot
-            if nums[i]==pivot and nums[j]==pivot:
-                i+=1
-                j-=1
-                continue
+            # # Both are pivot
+            # if nums[i]==pivot and nums[j]==pivot:
+            #     i+=1
+            #     j-=1
+            #     continue
 
-            #  either of them is pivot
-            if nums[i]==pivot: 
-                i+=1
-                if nums[j]>pivot:
-                    res[r] = nums[j]
-                    r-=1
-                j-=1
-                continue
+            # #  either of them is pivot
+            # if nums[i]==pivot: 
+            #     i+=1
+            #     if nums[j]>pivot:
+            #         res[r] = nums[j]
+            #         r-=1
+            #     j-=1
+            #     continue
                 
-            if nums[j]==pivot: 
-                j-=1
-                if nums[i]<pivot:
-                    res[l] = nums[i]
-                    l+=1
-                i+=1
-                continue
+            # if nums[j]==pivot: 
+            #     j-=1
+            #     if nums[i]<pivot:
+            #         res[l] = nums[i]
+            #         l+=1
+            #     i+=1
+            #     continue
 
             #  None of them is pivot
             if nums[i]<pivot:
