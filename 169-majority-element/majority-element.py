dfe1528fec57545c7b5f285cb39443e1153571cf
Shruti Varade <37963866+shrutivarade@users.noreply.github.com>
2025-07-19 10:43:22 -0400

    Time: 8 ms (37.65%) | Memory: 19.4 MB (19.13%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,9 +1,23 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         
+        # Simple Hashmap technique
         n = len(nums)
         hashmap = Counter(nums)
         for val, count in hashmap.items():
             if count > n/2:
                 return val
+
+        # two pointers for O(1) space;
+        nums.sort()
+        count =1
+        for i in range(1,len(nums)):
+            if nums[i] == nums[i-1]:
+                count +=1
+                if count>len(nums)/2:
+                    return nums[i]
+            else:
+                count = 1
+
+
 
