5980e4893fbb4df5ed3571c48ba69438d8c8c972
Shruti Varade <37963866+shrutivarade@users.noreply.github.com>
2025-07-19 09:40:16 -0400

    Time: 0 ms (100.00%) | Memory: 18 MB (11.14%) - LeetSync

--- 27-remove-element/remove-element.py
+++ 27-remove-element/remove-element.py
@@ -5,7 +5,8 @@ class Solution:
         
         while i<len(nums):
             if nums[i] == val:
-                nums[i:] = nums[i+1:]
+                # nums[i:] = nums[i+1:]
+                nums.pop(i)
                 continue
             i+=1
         return len(nums)
