5c489f8f71233221cd6a22a3cb9ec19cf40ca45b
Shruti Varade <37963866+shrutivarade@users.noreply.github.com>
2025-07-19 12:39:30 -0400

    Time: 0 ms (100.00%) | Memory: 18.9 MB (30.03%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -2,12 +2,18 @@ class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         # buy everyday and sell immediately at higher price
 
+        # profit = 0
+        # buy = prices[0]
+        # n = len(prices)
+        # for sell in range(0 , n):
+        #     if buy < prices[sell]: # check if we can sell when market open the next day.
+        #         profit += prices[sell] - buy
+        #     buy = prices[sell] # buy when market closes
+        # return profit
+
         profit = 0
-        buy = prices[0]
-        n = len(prices)
-        for sell in range(0 , n):
-            if buy < prices[sell]: # check if we can sell when market open the next day.
-                profit += prices[sell] - buy
-            buy = prices[sell] # buy when market closes
+        for i in range(1,len(prices)):
+            if prices[i]>prices[i-1]:
+                profit+=prices[i]-prices[i-1]
         return profit
 
