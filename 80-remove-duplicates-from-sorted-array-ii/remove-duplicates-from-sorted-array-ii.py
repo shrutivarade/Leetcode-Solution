8c1de55ac8c7acda1faf85302a391d19e7d36f6d
Shruti Varade <37963866+shrutivarade@users.noreply.github.com>
2025-07-19 10:23:21 -0400

    Time: 92 ms (24.05%) | Memory: 20.3 MB (96.02%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.py
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.py
@@ -2,27 +2,29 @@ class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
         # both the logic works
         
-        # i=0
-        # j=0
+        # extra space for count
+        i=0
+        j=0
+        while j< len(nums):
+            count = 1
+            while j+1 < len(nums) and nums[j] == nums[j+1]:
+                j+=1
+                count+=1
 
-        # while j< len(nums):
-        #     count = 1
-        #     while j+1 < len(nums) and nums[j] == nums[j+1]:
-        #         j+=1
-        #         count+=1
+            for k in range(min(2,count)):
+                nums[i] = nums[j]
+                i+=1
 
-        #     for k in range(min(2,count)):
-        #         nums[i] = nums[j]
-        #         i+=1
+            j+=1
+        return i
 
-        #     j+=1
-        # return i
+        # pop logic is computationally heavy
+        # i = 2
+        # while i < len(nums):
+        #     if nums[i] == nums[i - 2]:
+        #         nums.pop(i)
+        #     else:
+        #         i += 1
+        # return i+1
 
-        i = 2
-        while i < len(nums):
-            if nums[i] == nums[i - 2]:
-                nums.pop(i)
-            else:
-                i += 1
-        return i+1
         
