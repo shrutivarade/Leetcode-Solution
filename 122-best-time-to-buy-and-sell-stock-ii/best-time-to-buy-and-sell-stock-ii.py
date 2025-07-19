5534e859a987933bd3e71ebae80e3dd385ce9b3a
Shruti Varade <37963866+shrutivarade@users.noreply.github.com>
2025-07-19 12:37:22 -0400

    Time: 3 ms (38.98%) | Memory: 18.5 MB (99.78%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -6,8 +6,8 @@ class Solution:
         buy = prices[0]
         n = len(prices)
         for sell in range(0 , n):
-            if buy < prices[sell]: 
+            if buy < prices[sell]: # check if we can sell when market open the next day.
                 profit += prices[sell] - buy
-            buy = prices[sell]
+            buy = prices[sell] # buy when market closes
         return profit
 
