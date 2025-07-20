0089e16a1fc43482c7bdfa835200f91860291a17
Shruti Varade <37963866+shrutivarade@users.noreply.github.com>
2025-07-19 20:36:01 -0400

    Time: 8 ms (57.21%) | Memory: 18.5 MB (75.28%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.py
+++ 45-jump-game-ii/jump-game-ii.py
@@ -2,12 +2,14 @@ class Solution:
     def jump(self, nums: List[int]) -> int:
 
         l,r = 0,0
+        # l and r defines the window with start as l=r+1 and r=farthest
         jump=0
 
         while r<len(nums)-1:
             farthest = 0
             for i in range(l,r+1):
                 farthest = max(farthest, i+nums[i])
+            # update to next window
             l = r+1
             r = farthest
             jump+=1
